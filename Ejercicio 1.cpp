c53d59142e0ae93d6ce08d909bec84c33d525639
OscarMartinez13 <casillaso746@gmail.com>
2026-03-31 22:42:15 -0600

    Update Ejercicio 1.cpp

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -1,15 +1,1 @@
 
-#include <iostream>
-
-using namespace std;
-int main() {
-    int edad;
-    cout << "Ingresa tu edad: ";
-    cin >> edad;
-    if (edad >=18 ){
-        cout << "Eres mayor de edad. " << endl;
-    }  else {
-        cout << "Eres menor de edad, lo siento." << endl;
-    }
-    return 0;
-}
